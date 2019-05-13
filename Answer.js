84fe907e39e513b7b63611ce4e463a1719394888
Geanie Blanco <geanieblanco@gmail.com>
2019-05-13 02:04:27 -0400

    retest B

--- Answer.js
+++ Answer.js
@@ -1,25 +1,32 @@
-var $= function(selector){
+var $ = function (selector) {
     var elements = [];
 
+    //**********************************
+    // SOLUTION
+    // pass selector through a conditional
+    // statement that filters them by
+    // type and gets the correct elements.
+
+
     //Test A - Passed
-    if (selector == "div"){
+    if (selector == "div") {
         elements = document.getElementsByTagName("div");
     }
 
     //Test B - Failed
-    else if (selector == "img#some_other_id"){
-        elements.push(document.getElementById("some_other_id"));
+    else if (selector == "img.some_class") {
+        elements = document.getElementsByClassName("some_class");
     }
 
     //Test C - Passed
-    else if (selector == "#some_id"){
-        elements.push( document.getElementById("some_id"));
+    else if (selector == "#some_id") {
+        elements.push(document.getElementById("some_id"));
     }
 
     //Test D - Passed
-    else if (selector == '.some_class'){
-        elements= document.getElementsByClassName("some_class");
-    } 
+    else if (selector == '.some_class') {
+        elements = document.getElementsByClassName("some_class");
+    }
 
     //Test F - Passed
     else if (selector == "div#some_id.some_class") {
@@ -30,7 +37,7 @@ var $= function(selector){
     else if (selector == "div.some_class#some_id") {
         elements.push(document.getElementById("some_id"));
     }
-    
+
     //Test E - Passed
     else {
         elements.push()
